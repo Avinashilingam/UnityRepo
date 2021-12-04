7d7e9c7bcc953eaf0931e778ac2bce97e06ecfe4
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2021-12-04 16:53:55 +0530

    Worked on Bug fixes for sceneloader script

--- 2D_Sample_project/Assets/Scripts/SceneLoaderScript.cs
+++ 2D_Sample_project/Assets/Scripts/SceneLoaderScript.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneLoaderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Button button;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start() 
     {
-        
-    }
+     button.onClick.AddListener(OnButtonClick);
+    } 
+
+    private void OnButtonClick()
+    {
+      Debug.Log("Button Clicked");
+    }  
 }
+
